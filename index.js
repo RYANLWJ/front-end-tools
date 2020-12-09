1ce981be189983cce32175b89b9255bb469f97fd
RYAN LI <essentialmomo@icloud.com>
2020-12-09 15:27:38 +0800

    v13.0.0

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-if (process.env.NODE_ENV === 'production') { // 通过环境变量来决定入口文件
-  module.exports = require('./dist/tools.min.js')
+if (process.env.NODE_ENV === "production") {
+  // 通过环境变量来决定入口文件
+  module.exports = require("./dist/index.min.js");
 } else {
-  module.exports = require('./dist/tools.js')
+  module.exports = require("./dist/index.js");
 }
